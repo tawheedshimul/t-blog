5e39aec563471af229d48ee68ff78e963992a894
tawheedshimul <tawheedshimul@gmail.com>
2025-03-01 14:13:51 +0600

    upda

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { PostsModule } from './posts/posts.module';
 import { DatabaseModule } from './database/database.module';
 import { EmailModule } from './email/email.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [DatabaseModule, PostsModule, EmailModule],
+  imports: [DatabaseModule, PostsModule, EmailModule, ProfileModule],
 })
 export class AppModule {}
